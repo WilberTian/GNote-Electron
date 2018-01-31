0a32368cf7402abc256c937998e8127b7f4e5324
WilberTian <tianli917@hotmail.com>
2018-01-31 20:49:43 +0800

    enable devTool panel in development mode

--- main.js
+++ main.js
@@ -25,7 +25,9 @@ function createWindow () {
   
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === 'development') {
+   mainWindow.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
